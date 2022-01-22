20003374ba9e5305a97b25606490bdfa536b090d
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-22 22:04:25 +0100

    added onclick for game__button_reset

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const cells = document.querySelectorAll(".field__cell");
+const newGame = document.querySelector(".game__button_reset");
 
 for (const cell of cells) {
   cell.addEventListener("click", () => {
     document.getElementById(cell.id).style.background = "#ff0";
   });
 }
+
+newGame.addEventListener("click", () => {
+  for (const cell of cells) {
+    document.getElementById(cell.id).style.background = "#808080";
+  }
+});
