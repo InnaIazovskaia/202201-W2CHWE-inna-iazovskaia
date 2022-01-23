a66d65c685605e5cbdf8d1a638db28d2b5a616d8
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-23 15:13:49 +0100

    created variable cellsCount and function createField

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const cells = document.querySelectorAll(".field__cell");
 const resetGame = document.querySelector(".game__button_reset");
 const fieldArray = [];
 
+function creatField() {
+  fieldArray = [];
+}
+
 for (const cell of cells) {
   cell.addEventListener("click", () => {
     document.getElementById(cell.id).style.background = "#ff0";
