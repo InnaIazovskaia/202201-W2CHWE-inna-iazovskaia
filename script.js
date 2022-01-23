c4a62a16590ef2efca9886cbb2ac0f273af3ff87
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-23 15:34:16 +0100

    added cycles and condition to createField

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const cells = document.querySelectorAll(".field__cell");
 const resetGame = document.querySelector(".game__button_reset");
 const fieldArray = [];
+let cellsCount = 0;
 
-function creatField() {
+function createField() {
   fieldArray = [];
+  while (cellsCount < cells.length) {
+    for (let i = 0; i < 14; i++) {
+      const fieldArrayLine = [];
+      for (let j = 0; j < 20; j++) {
+        if (cells[cellsCount].style.background === "#ff0") {
+          fieldArrayLine.push(1);
+        } else {
+          fieldArrayLine.push(0);
+        }
+        cellsCount++;
+      }
+      fieldArray.push(fieldArrayLine);
+    }
+  }
+  return fieldArray;
 }
 
 for (const cell of cells) {
